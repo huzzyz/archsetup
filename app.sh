bbc06bdfd5ab2c66d64065ddebad22b177639b1c
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-16 00:48:58 +0400

    Create app.sh
    Personal list of apps to Install on refresh.

---
+++ app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+app_list="bluedevil bluez bluez-util wine winetricks discover smplayer smplayer-themes 
+plank bleachbit nasc google-chrome virt-manager libvirt qemu edk2-ovmf dnsmasq freerdp 
+ncdu discord conky conky-managerflameshot python-pip otpclient ulauncher notion-app-enhanced 
+masterpdfeditor-free freeoffice stacer-bin kindd teamviewer coretrl"
+
+yay -Sy $app_list --noconfirm
