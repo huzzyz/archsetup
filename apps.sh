f26bba2da38f073f10917d9b28aa751f9fad17ed
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-17 22:42:01 +0400

    Update apps.sh
    added timeshift

--- apps.sh
+++ apps.sh
@@ -2,6 +2,6 @@
 app_list="bluedevil wine winetricks discover smplayer smplayer-themes 
 plank bleachbit nasc google-chrome virt-manager libvirt qemu edk2-ovmf dnsmasq freerdp 
 ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
-masterpdfeditor-free freeoffice stacer-bin kindd teamviewer"
+masterpdfeditor-free freeoffice stacer-bin kindd teamviewer timeshift"
 
 yay -Sy $app_list --noconfirm
