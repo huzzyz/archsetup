14f8c7140bce17df758b5c1fe6bf9556044371f1
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-17 14:12:57 +0400

    Update apps.sh
    Updates

--- apps.sh
+++ apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 app_list="bluedevil wine winetricks discover smplayer smplayer-themes 
 plank bleachbit nasc google-chrome virt-manager libvirt qemu edk2-ovmf dnsmasq freerdp 
-ncdu discord conky conky-manager flameshot python-pip otpclient ulauncher notion-app-enhanced 
+ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
 masterpdfeditor-free freeoffice stacer-bin kindd teamviewer"
 
 yay -Sy $app_list --noconfirm
