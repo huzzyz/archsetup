fa9b9741825b3cf5d49e57ad602a193ffe3b5d62
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-18 17:07:29 +0400

    Update apps.sh
    added libinput-gestures and xdotool

--- apps.sh
+++ apps.sh
@@ -2,6 +2,6 @@
 app_list="bluedevil wine winetricks discover smplayer smplayer-themes 
 plank bleachbit nasc google-chrome virt-manager libvirt qemu edk2-ovmf dnsmasq freerdp 
 ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
-masterpdfeditor-free freeoffice stacer-bin kindd teamviewer timeshift"
+masterpdfeditor-free freeoffice stacer-bin kindd teamviewer timeshift xdotool libinput-gestures"
 
 yay -Sy $app_list --noconfirm
