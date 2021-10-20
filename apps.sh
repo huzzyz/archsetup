a3c89bfd6d1ef55eb204a8490e31881ba26b119f
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-20 19:34:50 +0400

    Update apps.sh
    typos

--- apps.sh
+++ apps.sh
@@ -3,6 +3,6 @@ app_list="bluedevil wine winetricks discover smplayer smplayer-themes
 plank bleachbit nasc google-chrome virt-manager libvirt qemu edk2-ovmf dnsmasq freerdp 
 ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
 masterpdfeditor-free stacer-bin kindd teamviewer timeshift xdotool libinput-gestures latte-dock
-plasma-integration"
+plasma-browser-integration"
 
 yay -Sy $app_list --noconfirm
