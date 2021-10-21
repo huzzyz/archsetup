ba0c73125d738cf99d9b0ba1a74f6937faa61b62
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-21 12:31:17 +0400

    apps_beast.sh
    removed vm related added running apps on completion

--- apps.sh
+++ apps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 app_list="bluedevil wine winetricks discover smplayer smplayer-themes 
-plank bleachbit nasc google-chrome virt-manager libvirt qemu edk2-ovmf dnsmasq freerdp 
-ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
+plank bleachbit nasc google-chrome ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
 masterpdfeditor-free stacer-bin kindd teamviewer timeshift xdotool libinput-gestures latte-dock
 plasma-browser-integration"
 
-yay -Sy $app_list --noconfirm
+yay -Sy $app_list --noconfirm && ./enable_apps.sh
