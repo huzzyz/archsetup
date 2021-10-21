5b74c927cbc023d68e5f8105df78cbbf1fce674a
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-21 12:34:44 +0400

    Update apps.sh
    re-added freeoffice

--- apps.sh
+++ apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 app_list="bluedevil wine winetricks discover smplayer smplayer-themes 
 plank bleachbit nasc google-chrome ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
-masterpdfeditor-free stacer-bin kindd teamviewer timeshift xdotool libinput-gestures latte-dock
+masterpdfeditor-free freeoffice stacer-bin kindd teamviewer timeshift xdotool libinput-gestures latte-dock
 plasma-browser-integration"
 
 yay -Sy $app_list --noconfirm && ./enable_apps.sh
