659ed3da41712af2367c7bb3b6b8c3712e837308
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-22 17:40:45 +0400

    Update apps.sh
    removed apps

--- apps.sh
+++ apps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-app_list="bluedevil wine winetricks discover smplayer smplayer-themes 
-plank bleachbit nasc google-chrome ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced 
-masterpdfeditor-free freeoffice stacer-bin kindd teamviewer timeshift xdotool libinput-gestures latte-dock
-plasma-browser-integration"
+app_list="bluedevil discover smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord conky conky-manager 
+flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin kindd 
+teamviewer timeshift xdotool libinput-gestures latte-dock plasma-browser-integration"
 
-yay -Sy $app_list --noconfirm && ./enable_apps.sh
+yay -Syyu $app_list --noconfirm && ./enable_apps.sh
