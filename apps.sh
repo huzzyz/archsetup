fb2cc00b504cb69598f7d35d4d031d0f6793e578
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-24 00:05:09 +0400

    Update apps.sh
    additions

--- apps.sh
+++ apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app_list="bluedevil discover smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord conky conky-manager 
-flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin kindd 
-teamviewer timeshift xdotool libinput-gestures plasma-browser-integration"
+flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin teamviewer backintime
+xdotool libinput-gestures plasma-browser-integration"
 
-yay -Syyu $app_list --noconfirm && ./enable_apps.sh
+yay -Syyu $app_list --noconfirm && chmod +x enable_apps.sh && ./enable_apps.sh
