7e0e22297f5d26b7f52344790d4b3c408629306c
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-27 18:20:14 +0400

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-app_list="bluedevil discover smplayer smplayer-themes bleachbit nasc google-chrome ncdu discord conky conky-manager 
-flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin teamviewer backintime
-xdotool libinput-gestures plasma-browser-integration"
+app_list="xorg sddm plasma breeze-gtk kdeconnect kde-gtk-config khotkeys kinfocenter kinit kio-fuse konsole kscreen okular plasma-desktop plasma-disks plasma-nm plasma-pa powerdevil print-manager sddm-kcm solid xsettingsd google-chrome teamviewer discover smplayer smplayer-themes bleachbit nasc ncdu discord conky conky-manager flameshot python-pip otpclient ksysguard notion-app-enhanced masterpdfeditor-free freeoffice stacer-bin teamviewer backintime tesseract tesseract-data-eng leptonica xdotool libinput-gestures plasma-browser-integration"
 
 yay -Syyu $app_list --noconfirm && chmod +x enable_apps.sh && ./enable_apps.sh
